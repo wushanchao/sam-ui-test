19afc27ffdf529450d4d40e224485ec9a25b8e35
wushanchao <wushanchao@gongdao.com>
2021-05-20 19:52:07 +0800

    feat: demo改造

---
+++ demo/baidu.test.js
@@ -0,0 +1,64 @@
+const suite = require("../src/index");
+console.log("suite", suite);
+suite({
+  name: "百度搜索场景",
+  browser: {
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    headless: false,
+    slowMo: 200,
+  },
+  step: [
+    {
+      name: "获取百度搜索的第一个结果标题",
+      action: [
+        {
+          method: "goto",
+          arg: ["https://www.baidu.com"],
+        },
+        {
+          method: "waitForSelector",
+          arg: ["#kw"],
+        },
+        {
+          method: "bringToFront",
+        },
+        {
+          method: "type",
+          arg: ["#kw", "google"],
+        },
+        {
+          method: "click",
+          arg: ["input[type='submit']"],
+        },
+        {
+          method: "$eval",
+          arg: [
+            'div[id="1"] em',
+            function (h1) {
+              return h1.innerHTML;
+            },
+          ],
+          name: "text",
+        },
+        {
+          method: "expect",
+          arg: ["$$.text", "toBe", "google谷歌"],
+        },
+      ],
+    },
+    {
+      name: "点击第一条链接",
+      action: [
+        {
+          method: "waitForSelector",
+          arg: ["h3.t > a"],
+        },
+        {
+          method: "click",
+          arg: ["h3.t:nth-of-type(1) > a"],
+        },
+      ],
+    },
+  ],
+});
