4f79f4c80a5973c6150bca353d3dfccc1782b306
wushanchao <wushanchao@gongdao.com>
2021-05-21 16:35:58 +0800

    feat: 文档变更

--- README.md
+++ README.md
@@ -1,123 +1,123 @@
 # sam-ui-test
-组合Chrome，Jest，Puppeteer的自动化UI业务测试配置库。
 
-# 背景
-Puppeteer的作为
+sam-ui-test是一个组合 Chrome，Jest，Puppeteer 的自动化UI测试配置库。  
+它通过面向配置编程的思路，结合Jest的断言能力和Puppeteer控制Chrome的能力，来降低部分场景下的自动化UI测试的上手成本。
 
 # 使用
+
 ### 原理  
-对场景测试行为进行抽象，再进行封装： 
+
+基于Jest对UI测试场景进行抽象封装测试用例：
+
 ```
 场景，步骤，操作行为，期望结果
 ```
 
-### 步骤
-1. 安装jest
-```
-npm install jest --save
-```
+### 使用步骤
 
-2. 安装puppeteer
-```
-npm install puppeteer --ignore-scripts --save
-```
-`--ignore-scripts`这个参数是为了安装puppeteer时，跳过Chromium的下载。  
+1. 安装 sam-ui-test
 
-3. 安装sam-ui-test
 ```
 npm install sam-ui-test --save
 ```
 
-4. 新建测试文件`ui.test.js`
-并添加内容如下。  
-```javascript
-const suite = require('sam-ui-test');
-suite({
-    name:'百度搜索场景',
-    browser: {
-        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-        headless: false,
-        slowMo: 200,
-    },
-    step: [
-    {
-        name: '获取百度搜索的第一个结果标题',
-        action: [
-            {
-                method: 'goto',
-                arg: ['https://www.baidu.com']
-            },
-            {
-                method: 'waitForSelector',
-                arg: ['#kw']
-            },
-            {
-                method: 'bringToFront',
-            },
-            {
-                method: 'type',
-                arg: ['#kw', 'google']
-            },
-            {
-                method: '$eval',
-                arg: ['div[id="1"] em', function (h1) {
-                    return h1.innerHTML;
-                }],
-                name: 'text',
-            },
-            {
-                method: 'expect',
-                arg: ['$$.text', 'toBe', 'Google'],
-            }
-        ],
-    },
-    {
-        name: '点击第一条链接',
-        action: [
-            {
-                method: 'waitForSelector',
-                arg: ['h3.t > a']
-            },
-            {
-                method: 'click',
-                arg: ['h3.t:nth-of-type(1) > a']
-            }
-        ]
+2. 新建测试文件`ui.test.js`
+   并添加内容如下。
 
-    }
-]
+```javascript
+const suite = require("sam-ui-test");
+suite({
+  name: "百度搜索场景",
+  browser: {
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    headless: false,
+    slowMo: 200,
+  },
+  step: [
+    {
+      name: "获取百度搜索的第一个结果标题",
+      action: [
+        {
+          method: "goto",
+          arg: ["https://www.baidu.com"],
+        },
+        {
+          method: "waitForSelector",
+          arg: ["#kw"],
+        },
+        {
+          method: "bringToFront",
+        },
+        {
+          method: "type",
+          arg: ["#kw", "google"],
+        },
+        {
+          method: "$eval",
+          arg: [
+            'div[id="1"] em',
+            function (h1) {
+              return h1.innerHTML;
+            },
+          ],
+          name: "text",
+        },
+        {
+          method: "expect",
+          arg: ["$$.text", "toBe", "Google"],
+        },
+      ],
+    },
+    {
+      name: "点击第一条链接",
+      action: [
+        {
+          method: "waitForSelector",
+          arg: ["h3.t > a"],
+        },
+        {
+          method: "click",
+          arg: ["h3.t:nth-of-type(1) > a"],
+        },
+      ],
+    },
+  ],
 });
 ```
 
-5. 更改package.json文件
+3. 更改 package.json 文件
+
 ```javascript
 "scripts": {
     "test": "jest ./ui.test.js"
 },
 ```
 
-6. 执行测试命令并观察测试结果
+4. 执行测试命令获取测试结果
+
 ```
 npm run test
 ```
 
 ### 解释
-action里面的method方法默认是puppeteer的page实例方法。具体可参考[这个链接](https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#class-page)。  
-当method方法是expect时，expect是jest的expect方法。  
+
+action 里面的 method 方法默认是 puppeteer 的 page 实例方法。具体可参考[这个链接](https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#class-page)。  
+当 method 方法是 expect 时，expect 是 jest 的 expect 方法。
 
 ### 代码发布
+
 1. 执行`npm login --registry http://registry.npmjs.org`登录账号
-2. 修改package.json版本号，通常+1
+2. 修改 package.json 版本号，通常+1
 3. 执行`npm publish --registry http://registry.npmjs.org`发布
 
+### 项目规划
 
-### 自动化测试规划
 0. 配置化基本功能完善
-场景变量，行动变量的支持
-配置参数校验
-自定义action函数
-1. 配置化转DSL化
-2. 自动化接口测试和自动化UI测试的接口一致化
+   场景变量，行动变量的支持
+   配置参数校验
+   自定义 action 函数
+1. 配置化转 DSL 化
+2. 自动化接口测试和自动化 UI 测试的接口一致化
 3. 并行测试
 4. 探索需求变化引起的测试用例变化同步更新的更好策略
-
--- src/index.js
+++ src/index.js
@@ -1,128 +1,127 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require("puppeteer-core");
 
-process.once('SIGINT', () => {
-    process.exit(1);
+process.once("SIGINT", () => {
+  process.exit(1);
 });
 
-const samui = (suiteObj={}) => {
-    // 验证
-    const needKeyObj = {
-        name: 'name is required',
-        browser:'browser is required',
-        step:'step is required'
+const variableTempalte = (str, obj) => {
+  // 如果名称以$$.开头意味着这是一个上下文变量。
+  if (
+    Object.prototype.toString.call(str) === "[object String]" &&
+    str.slice(0, 3) === "$$."
+  ) {
+    const remainArr = str.slice(3).split(".");
+    let temp = obj;
+
+    // 返回数组最后一个值
+    for (let i of remainArr) {
+      temp = obj[i];
     }
-    Object.keys(needKeyObj).map((item)=>{
-        if(suiteObj[item] === undefined){
-            throw needKeyObj[item]
-        }
-    });
-
-    describe(suiteObj.name, function () {
-        let browser;
-        let page;
-
-        beforeAll(async () => {
-            browser = await puppeteer.launch(suiteObj.browser);
-            page = await browser.newPage();
-        });
-
-        afterAll(() => {
-            browser.close();
-        }, 2000);
-
-
-        const variableTempalte = function (str, obj) {
-            // 如果名称以$$.开头意味着这是一个上下文变量。
-            if (Object.prototype.toString.call(str) === '[object String]' && str.slice(0, 3) === '$$.') {
-                const remainArr = str.slice(3).split('.');
-                let temp = obj;
-
-                // 返回数组最后一个值
-                for (let i of remainArr) {
-                    temp = obj[i];
-                }
-                return temp;
-            }
-            else {
-                return str;
-            }
-        };
-
-        const transformAction = function () {
-            return async function (obj, actionScope) {
-                let arg = obj['arg'] || [];
-
-                // 必要的校验
-                if(obj.method === undefined){
-                    throw ("action item's method is required")
-                }
-
-                // 对arg数组里面的值进行转义
-                for (let i = 0; i < arg.length; i++) {
-                    let item = arg[i];
-
-                    // todo 获取局部变量方式优化
-                    // todo 获取全局变量获取方式
-                    arg[i] = variableTempalte(item, actionScope);
-                }
-
-                // action默认上下文是page
-                let context = obj.context || page;
-
-                // 对method为except进行特别处理
-                if (obj.method === 'expect') {
-                    expect(arg[0])[arg[1]](arg[2]);
-                }
-                else {
-                    const name = obj['name'];
-                    const fn = async function () {
-                        return await context[obj.method].apply(context, obj.arg);
-                    }
-
-                    // action若有name，便存储返回值，方便后面action使用。
-                    if (name) {
-                        actionScope[name] = await fn();
-                    }
-                    else {
-                        await fn();
-                    }
-                }
-
-            };
-        };
-
-        const transformStep = function (step) {
-            return async function () {
-                let context = step.context || page;
-                global.test(step.name, async function () {
-                    let action = step.action;
-                    let tempAction = [];
-                    let scope = {};
-                    for (let item of action) {
-                        tempAction.push(transformAction());
-                    }
-
-                    for (let i = 0; i < tempAction.length; i++) {
-                        let actionItem = action[i];
-                        await tempAction[i](actionItem, scope);
-                    }
-
-
-                    // 取变量值。$$为action变量，$为场景变量
-                }, 10000);
-            };
-        };
-
-        const transformScene = function (stepArr) {
-            for (let step of stepArr) {
-                transformStep(step)();
-            }
-        };
-
-        transformScene(suiteObj.step);
-    });
+    return temp;
+  } else {
+    return str;
+  }
 };
 
+const samui = (suiteObj = {}) => {
+  // 验证
+  const needKeyObj = {
+    name: "name is required",
+    browser: "browser is required",
+    step: "step is required",
+  };
+  Object.keys(needKeyObj).map((item) => {
+    if (suiteObj[item] === undefined) {
+      throw needKeyObj[item];
+    }
+  });
 
+  describe(suiteObj.name, function () {
+    let browser;
+    let page;
+
+    beforeAll(async () => {
+      browser = await puppeteer.launch(suiteObj.browser);
+      page = await browser.newPage();
+    });
+
+    afterAll(() => {
+      browser.close();
+    }, 2000);
+
+    const transformAction = () => {
+      return async function (obj, actionScope) {
+        let arg = obj["arg"] || [];
+
+        // 必要的校验
+        if (obj.method === undefined) {
+          throw "action item's method is required";
+        }
+
+        // 对arg数组里面的值进行转义
+        for (let i = 0; i < arg.length; i++) {
+          let item = arg[i];
+
+          // todo 获取局部变量方式优化
+          // todo 获取全局变量获取方式
+          arg[i] = variableTempalte(item, actionScope);
+        }
+
+        // action默认上下文是page
+        let context = obj.context || page;
+
+        // 对method为except进行特别处理
+        if (obj.method === "expect") {
+          expect(arg[0])[arg[1]](arg[2]);
+        } else {
+          const name = obj["name"];
+          const fn = async function () {
+            return await context[obj.method].apply(context, obj.arg);
+          };
+
+          // action若有name，便存储返回值，方便后面action使用。
+          if (name) {
+            actionScope[name] = await fn();
+          } else {
+            await fn();
+          }
+        }
+      };
+    };
+
+    const transformStep = (step) => {
+      return async function () {
+        let context = step.context || page;
+        global.test(
+          step.name,
+          async function () {
+            let action = step.action;
+            let tempAction = [];
+            let scope = {};
+            for (let item of action) {
+              tempAction.push(transformAction());
+            }
+
+            for (let i = 0; i < tempAction.length; i++) {
+              let actionItem = action[i];
+              await tempAction[i](actionItem, scope);
+            }
+
+            // 取变量值。$$为action变量，$为场景变量
+          },
+          10000
+        );
+      };
+    };
+
+    const transformScene = (stepArr) => {
+      for (let step of stepArr) {
+        transformStep(step)();
+      }
+    };
+
+    transformScene(suiteObj.step);
+  });
+};
 
 module.exports = samui;
